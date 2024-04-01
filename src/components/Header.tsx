41f2586dc215c1546d1d5e9148ef0a6635f49e8a
RaphaelMarquesMartorella <raphael.martorella@hotmail.com>
2024-04-01 17:35:44 -0300

    add: responsivity feature

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Header() {
     return (
         <main>
-            <header className="flex  bg-[#231C61] mt-9 ml-9 mr-9 h-[70px] rounded-full">
+            <header className="bg-[#231C61] mt-9 ml-9 mr-9 h-[70px] rounded-full">
                 <div id="left-section">
                     <Image 
                         className="pl-4"
@@ -13,20 +13,20 @@ export default function Header() {
                         height={60}
                     />  
                 </div>
-                <div id="right-section" className="flex ml-[950px] pt-6">
-                <a href="">
-                    <h3 className=" text-[#CB89D3] font-raleway text-lg">Home</h3>
-                </a>
-                <a href="">
-                    <h3 className="pl-[130px] text-[#7B3397] font-raleway text-lg">Sobre</h3>
-                </a>
-                <a href="">
-                    <h3 className="pl-[130px] text-[#7B3397] font-raleway text-lg">Mentores</h3>
-                </a>
-                <a href="">
-                    <h3 className="pl-[130px] text-[#7B3397] font-raleway text-lg">Contato</h3>
-                </a>
-                    </div>
+                <div id="right-section" className="flex ml-32 sm:ml-64 md:ml-[250px] lg:ml-[400px] xl:ml-[950px] mt-[-40px]">
+    <a href="">
+        <h3 className="text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl text-[#CB89D3] font-raleway">Home</h3>
+    </a>
+    <a href="">
+        <h3 className="pl-8 sm:pl-6 md:pl-12 lg:pl-24 xl:pl-32 text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl text-[#7B3397] font-raleway">Sobre</h3>
+    </a>
+    <a href="">
+        <h3 className="pl-8 sm:pl-6 md:pl-12 lg:pl-24 xl:pl-32 text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl text-[#7B3397] font-raleway">Mentores</h3>
+    </a>
+    <a href="">
+        <h3 className="pl-8 sm:pl-6 md:pl-12 lg:pl-24 xl:pl-32 text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl text-[#7B3397] font-raleway">Contato</h3>
+    </a>
+</div>
             </header>
         </main>
     );
--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -12,8 +12,8 @@ export default function Section() {
           objectFit="cover"
         />
         <div className="relative z-10 flex flex-col justify-center h-full w-full">
-          <h1 className="text-7xl text-center font-bold mb-10">Transformando a Pesquisa</h1>
-          <h3 className="text-xl text-center px-10">
+          <h1 className="text-2xl sm:text-4xl text-center md:text-4xl lg:text-5xl xl:text:7xl font-bold mb-10">Transformando a Pesquisa</h1>
+          <h3 className="text-sm sm:text-base text-center md:text-lg lg:text-xl xl:text-2xl px-10">
             Olá! Somos o Instituto Imprinting, uma startup de DAO dedicada a<br/>
             revolucionar a pesquisa científica e o desenvolvimento humano.
           </h3>
